a55b69729bc6e6ef12f8a2b866f3d5d79fdd8519
Derek Dyer <derek@digitalderek.com>
2015-07-10 20:11:39 -0400

    kicking the tires on dashboard

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,6 +1,12 @@
 class ListsController < ApplicationController
   def index
+    @user = current_user.name
+  end
 
+  private
+
+  def link_params
+    params.require(:lists).permit(:name, :user_id)
   end
 
 end
