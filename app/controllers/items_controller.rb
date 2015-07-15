47440577907e819c0b8bef76340d554dc0c0b354
Derek Dyer <derek@digitalderek.com>
2015-07-15 14:20:06 -0400

    Kids, Lists, and Item views hooked up to the database

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,31 @@
 class ItemsController < ApplicationController
-  
-  def index
 
+ def index
+  @items = Item.all
+  @item = Item.new
+end
+
+def new
+
+end
+
+def create
+  @item = Item.new(item_params)
+
+  respond_to do |format|
+    if @item.save
+      format.html { redirect_to items_path, notice: 'You added a new Item, Great job!' }
+    else
+      format.html { render :new }
+    end
+  end
+end
+
+private
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def item_params
+    params.require(:item).permit(:task, :list_id, :due_date, :is_complete)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150708204914) do
+ActiveRecord::Schema.define(version: 20150715030034) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20150708204914) do
 
   create_table "kids", force: :cascade do |t|
     t.integer "user_id"
+    t.string  "name"
   end
 
   add_index "kids", ["user_id"], name: "index_kids_on_user_id", using: :btree
