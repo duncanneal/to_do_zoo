fd45b38f1a333b43e003b03086fd137de4ac9d9c
Derek Dyer <derek@digitalderek.com>
2015-07-22 16:58:05 -0400

    item images and delete

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -28,11 +28,28 @@ class ItemsController < ApplicationController
     redirect_to kids_path, notice: 'Great Job!'
   end
 
+   def destroy
+    @item = Item.find(params[:id])
+    
+
+    @item.destroy
+    respond_to do |format|
+      format.html { redirect_to lists_url, notice: 'Post was successfully destroyed.' }
+    end
+
+ #   if @item.destroy
+ #     format.html { redirect_to list_items_path(@list), notice: "You have successfully removed this item." }
+ #   else
+ #     format.html { redirect_to list_items_path(@list), notice: "Item could not be deleted." }
+ #   end
+    #redirect_to @todo_list   
+  end  
+
   private
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def item_params
-    params.require(:item).permit(:task, :list_id, :due_date, :is_complete)
+    params.require(:item).permit(:task, :list_id, :due_date, :is_complete, :item_image)
   end
 
   def set_list
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,5 @@ class Item < ActiveRecord::Base
   def completed?
     !completed_at.blank?
   end
+  mount_uploader :item_image, ItemImageUploader
 end
